018b3301d61711e1c938fa00289e22b9521e4644
Miley <miley.ho@hyweb.com.tw>
2022-09-20 10:57:22 +0800

    first commit

---
+++ js/customize.js
@@ -0,0 +1,120 @@
+//自行加入的JS請寫在這裡
+;
+$(function() {
+    $('.singleSlider').slick({
+        dots: true,
+        arrow: true,
+        infinite: true,
+        speed: 500,
+        autoplay: true,
+        fade: true,
+        cssEase: 'ease'
+    });
+    $('.adSlider .container').slick({
+        dots: false,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay: true,
+        arrow: true,
+        responsive: [{
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 4,
+                slidesToScroll: 4,
+                infinite: true,
+                dots: false,
+                arrows: true
+            }
+        }, {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                arrows: true
+            }
+        }, {
+            breakpoint: 575,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2,
+                arrows: true
+            }
+        }]
+    });
+    $('.cp_slider').slick({
+        dots: false,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        autoplay: false,
+        autoplaySpeed: 1500,
+        pauseOnHover: true,
+        pauseOnFocus: true,
+        focusOnSelect: true,
+        accessibility: true,
+        responsive: [{
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+            }
+        }, {
+            breakpoint: 600,
+            settings: {
+                arrows: true,
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+        }, {
+            breakpoint: 480,
+            settings: {
+                arrows: true,
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }]
+    });
+    // sync slider
+    $('.Slider-for').on('init reInit afterChange', function(event, slick, currentSlide) {
+        var i = (currentSlide ? currentSlide : 0) + 1;
+        $('.controls').html(i + '/' + slick.slideCount);
+    });
+    $('.Slider-for').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: true,
+        fade: true,
+        asNavFor: '.Slider-nav',
+        autoplay: true,
+        autoplaySpeed: 2000,
+        infinite: true
+    });
+    $('.Slider-nav').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        asNavFor: '.Slider-for',
+        dots: true,
+        arrows: true,
+        focusOnSelect: true,
+        infinite: true
+        // centerMode: true,
+    });
+    // 進階查詢
+    $(window).bind('resize load', function(e) {
+        var windowWidth = $(window).width();
+        if (windowWidth <= 1024) {
+            $('.advance_search button').off().click(function(e) {
+                $('.advance_block').stop(true, true).slideToggle();
+            });
+        } else {
+            $('.advance_search button').click(function(e) {
+                $('.advance_block').stop(true, true).slideToggle();
+            });
+        }
+    });
+});
